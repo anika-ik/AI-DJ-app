fcb59aa4ad5497ae3fa74cf086a354e7434631f8
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-09-10 16:21:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song="";
+
+function preload(){
+    song= loadsound("music.mp3")
+
+}
+
+function setup(){
+    canvas= createCanvas(600, 500);
+    canvas.center();
+
+    video= creareCapture(VIDEO);
+    video.hide()
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+
+}
+
+function play(){
+    song.play();
+}
