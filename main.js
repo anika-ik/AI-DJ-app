d9455ec154dc278c4631350f035dbde6fd1dfdcd
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-09-13 15:26:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX= 0;
+leftWristY= 0;
+rightWristX= 0;
+rightWristY= 0;
 
 function preload(){
     song= loadSound("music.mp3")
@@ -10,7 +14,14 @@ function setup(){
     canvas.center();
 
     video= createCapture(VIDEO);
-    video.hide()
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("posenet is intialized");
 }
 
 function draw(){
@@ -20,4 +31,19 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY= " + leftWristY);
+
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("rightWristX= " + rightWristX + " rightWristY= " + rightWristY);
+    }
 }
