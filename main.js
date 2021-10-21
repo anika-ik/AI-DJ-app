36d9aa0ce9b1f5727a5ee6c461fbe04ab8358c13
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-10-21 17:30:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreleftWrist = 0;
+scorerightWrist = 0;
 
 function preload() {
     song = loadSound("music.mp3")
@@ -31,6 +32,32 @@ function draw() {
     fill("#9e050a");
     stroke("#9e050a");
 
+    if (scorerightWrist > 0.2) {
+
+        circle(rightWristX, rightWristY, 20);
+
+        if (rightWristY > 0 && rightWristY <= 100) {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
+        else if (rightWristY > 100 && rightWristY <= 200) {
+            document.getElementById("speed").innerHTML = "speed = 1x";
+            song.rate(1);
+        }
+        else if (rightWristY > 200 && rightWristY <= 300) {
+            document.getElementById("speed").innerHTML = "speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if (rightWristY > 300 && rightWristY <= 400) {
+            document.getElementById("speed").innerHTML = "speed = 2x";
+            song.rate(2);
+        }
+        else if (rightWristY > 400 && rightWristY <= 500) {
+            document.getElementById("speed").innerHTML = "speed = 2.5x";
+            song.rate(2.5);
+        }
+    }
+
     if (scoreleftWrist > 0.2) {
         circle(leftWristX, leftWristY, 20);
         inNumberLeftWristY = Number(leftWristY);
@@ -51,8 +78,10 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scorerightWrist = results[0].pose.keypoints[10].score;
         scoreleftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreleftWrist= " + scoreleftWrist);
+        console.log("scoreleftWrist= " + scoreleftWrist + " scorerightWrist= " + scorerightWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY= " + leftWristY);
